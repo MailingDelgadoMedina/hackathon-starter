4532f453071df454ca70fa23f929d2bb1afbb329
Mailing <mailing.delgado.scitexas@gmail.com>
2020-10-11 17:51:38 -0400

    updated deploy.sh eliminated mongo code

--- deploy.sh
+++ deploy.sh
@@ -5,12 +5,3 @@ npm install node-ssh
 echo "Running 'deploy.js'"
 node deploy.js
 
-sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 0C49F3730359A14518585931BC711F9BA15703C6
-
-echo "deb [ arch=amd64,arm64 ] http://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/3.4 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-3.4.list
-
-sudo apt-get update
-
-sudo apt-get install -y mongodb-org
-
-sudo service mongod start
